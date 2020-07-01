c3a0976ce7f1fb96d193528c455f3d2401ea5631
James Ye <998065+zhaoyun95@users.noreply.github.com>
2020-07-01 14:43:14 -0700

    data loading completed

--- database-setup.py
+++ database-setup.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 import pandas as pd
 import psycopg2
+import glob
 
 engine = create_engine(f'postgresql://{db_username}:{db_password}@localhost:5432/StocksDataBase')
 connection = engine.connect()
@@ -34,9 +35,71 @@ def executeScriptsFromFile(filename):
         except OperationalError as msg:
             print ("Command skipped: ", msg)
 
-# create tables
+# --- Step One: create tables
 executeScriptsFromFile('createDatabase.sql')
 
-# load csv into tables using Jupyter Notebook
-# we could also copy jupyter notebooks code here after everything works
+# load data files into database
 
+# --- Step Two: Load company.xlsx ---
+# show database tables
+print(engine.table_names())
+
+# read company.xlsx
+company_df = pd.read_excel("data/company.xlsx");
+print(company_df.head())
+
+# rerange column position to match database table columns
+company_df = company_df[["ticker","name", "ranking", "mkt_cap", "pe_ratio", "eps", "dividend_pct", "exchange", "esg_score", "recom_rating", "sector", "industry", "country", "city", "latitude", "longitude"]]
+print(company_df.head())
+
+# remove old company data
+engine.execute("delete from company")
+
+# load company dataframe into database
+company_df.to_sql(name='company', con=engine, if_exists='append', chunksize = 20, index=False)
+company_df2 = pd.read_sql("select * from company", con=engine)
+print("company data after loading")
+print(company_df2.count())
+
+
+# --- Step Three: Load price csv files ---
+priceCSVFiles = glob.glob("data/*.csv")
+print(priceCSVFiles)
+
+# remove old price data
+engine.execute("delete from price")
+
+# loop through all data/*.csv files and load them into price table
+for file in priceCSVFiles:
+    file = file.strip()
+    
+    print(file)
+    price_df = pd.read_csv(file)
+    # print(price_df.head())
+    csv_file = file.split('\\')[1]
+    
+    # ignore company.csv, we will handle it differetly
+    if csv_file == "company.csv":
+        continue
+
+    ticker = csv_file.strip('.csv')
+    price_df["ticker"] = ticker
+   
+    
+    # rename column heading to match database table columns
+    price_df.rename(columns = {"Date":"date", "Open":"open", "High":"high", "Low":"low", "Close":"close", "Adj Close":"adj_close", "Volume":"volume"}, inplace=True)
+    # make ticker as the first column
+    price_df = price_df[["ticker", "date", "open", "high", "low", "close", "adj_close", "volume"]]
+    
+    # load dataframe into database
+    try:
+        print(f"loading {ticker}")
+        price_df.to_sql(name='price', con=engine, if_exists='append', chunksize = 20, index=False)
+    except Exception as e:
+        print(e)
+
+# check out the price table
+price_df2 = pd.read_sql("select * from price", con=engine)
+print("price table after loading:")
+print("column, count")
+print(price_df2.count())
