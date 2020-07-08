d431a1d3efb994c582fac7fb62131ee3b7631c4e
James Ye <998065+zhaoyun95@users.noreply.github.com>
2020-07-07 20:58:33 -0700

    added paul's piechart

--- stock_inspector/static/js/landingpage.js
+++ stock_inspector/static/js/landingpage.js
@@ -76,3 +76,64 @@ d3.json(url).then(function(tableData) {
         });
     };
 });
+
+const url2 = "/api/v1.0/company";
+d3.json(url).then(function(data) {
+    // sort data by company name
+    data.sort(function(a,b){
+        if (a.name < b.name) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
+    console.log(data);
+
+    createPieChart(data);
+});
+
+
+// create a pie chart of sectors
+function createPieChart(data){
+    console.log(data);
+
+    sectors = data.map(d=>d.sector);
+
+    sectorCounts = {};
+
+    sectors.forEach(function(d){
+        if (sectorCounts[d]) {
+            sectorCounts[d] += 1;
+        } else {
+            sectorCounts[d] = 1;
+        }
+    })
+
+    console.log(sectorCounts);
+    
+    var labels = [];
+    var values = [];
+
+    Object.entries(sectorCounts).forEach(([key, value]) => {
+        labels.push(key);
+        values.push(value);
+    });
+
+    console.log(labels);
+    console.log(values);
+
+    var trace1 = {
+        labels: labels,
+        values: values,
+        type: 'pie'
+        };
+    
+        var data = [trace1];
+    
+        var layout = {
+        title: "'Sector' Chart",
+        };
+    
+        Plotly.newPlot("pieChart", data, layout);
+};
